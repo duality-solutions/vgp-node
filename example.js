37ee5d995dc4247207fb8187f2d679c9e307a82a
Amir Abrams <amirabrams@mail.com>
2020-09-06 02:41:20 -0500

    Update buffers passed to VGP functions

--- example.js
+++ example.js
@@ -5,7 +5,7 @@
     node-gyp configure
     node-gyp build --debug
 */
-var vgp = require('./build/Debug/vgp.node');
+var vgp = require('./');
 
 // VGP Encrypt using multiple keys
 // http://ed25519.herokuapp.com/
@@ -23,11 +23,12 @@ var vgp = require('./build/Debug/vgp.node');
 
 var publicKeys = ["1a7af93142337fc2e6696c771414c703f69bf17b273dba9d51993d28576d5cb9", "bcd2d75acc94b4cbc440908250e14207ecee4e1e1aa9cc5479dfb07584e38e34"];
 var message = 'VGP is easy to use!';
-var cipherText = vgp.Encrypt(new Buffer(publicKeys, 'utf8'), new Buffer(message, 'utf8'));
+var cipherText = vgp.Encrypt(Buffer.from(publicKeys, 'utf8'), Buffer.from(message, 'utf8'));
+
 
 // VGP decrypt example using the first private key
 var privateSeed1 = "23ce201beb9d303ae3b5ae80f703837f1c1c312c6af2fe25f0ebbf2a67a2fbbe";
-var decryptedMessage1 = vgp.Decrypt(new Buffer(privateSeed1, 'utf8'), new Buffer(cipherText, 'utf8'));
+var decryptedMessage1 = vgp.Decrypt(Buffer.from(privateSeed1, 'utf8'), Buffer.from(cipherText, 'utf8'));
 if (message == decryptedMessage1) {
     console.log('VGP encrypt/decrypt with first private key successful!');
 } else {
@@ -36,7 +37,7 @@ if (message == decryptedMessage1) {
 
 // VGP decrypt example using the second private key
 var privateSeed2 = "ba858559258f3b75539d4b23de907d82cc83d49988b2f346d51141775dd76108";
-var decryptedMessage2 = vgp.Decrypt(new Buffer(privateSeed2, 'utf8'), new Buffer(cipherText, 'utf8'));
+var decryptedMessage2 = vgp.Decrypt(Buffer.from(privateSeed2, 'utf8'), Buffer.from(cipherText, 'utf8'));
 if (message == decryptedMessage2) {
     console.log('VGP encrypt/decrypt with second private key successful!');
 } else {
--- vgp.cc
+++ vgp.cc
@@ -1,122 +1,80 @@
 #include <node.h>
 #include <node_buffer.h>
-#include <v8.h>
-#include <stdint.h>
-#include "nan.h"
+
+#include <nan.h>
+#include <stdlib.h>
 
 extern "C" {
     #include "vgp/include/encryption.h"
 }
 
-#define THROW_ERROR_EXCEPTION(x) Nan::ThrowError(x)
-#define THROW_ERROR_EXCEPTION_WITH_STATUS_CODE(x, y) NanThrowError(x, y)
-
 using namespace node;
 using namespace v8;
 
-/**
- * @brief Encrypts a piece of data using BDAP for a set of recipient's public-keys.
- * 
- * @param vchPubKeys The set of recipients Ed25519 public-keys, 32 bytes each
- * @param vchData The input data to be encrypted
- * @param vchCipherText The output ciphertext
- * @param strErrorMessage The string containing error-message in the event of failure
- * @return true on success
- * @return false on failure
-
+/*
 bool EncryptBDAPData(const vCharVector& vchPubKeys,
                      const CharVector& vchData,
                      CharVector& vchCipherText,
                      std::string& strErrorMessage);
- */
-void encrypt(const v8::FunctionCallbackInfo<v8::Value>& args) {
-    v8::Isolate* isolate = args.GetIsolate();
-
-    if (args.Length() < 2) {
-       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
-       return;
-    }
-    /*
-    Local<Object> objPubKeys = args[0]->ToObject();
-    if(!Buffer::HasInstance(objPubKeys)) {
-       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate,"Argument should be a buffer object.")));
-       return;
-    }
-    const std::vector<std::vector<unsigned char>> vPubKeys = (std::vector<std::vector<unsigned char>>)Buffer::Data(objPubKeys);
-
-    Local<Object> objData = args[1]->ToObject();
-    if(!Buffer::HasInstance(objPubKeys)) {
-       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate,"Argument should be a buffer object.")));
-       return;
-    }
-    const std::vector<unsigned char> vchData = (std::vector<unsigned char>)Buffer::Data(objData);
-    
-    std::vector<unsigned char> output;
-    std::string strErrorMessage
-    if !(EncryptBDAPData(vPubKeys, vchData, output, strErrorMessage)) {
-        isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, strErrorMessage)));
-        return;
-    }
-    */
-    // return raw vchCipherText
-    unsigned char* output = (unsigned char*) malloc(sizeof(unsigned char) * 32);
-    v8::Local<v8::Value> returnValue = Nan::CopyBuffer(((char*)output), 32).ToLocalChecked();
-    
-    args.GetReturnValue().Set(returnValue);
-}
+*/
+ 
 /**
- * @brief Decrypts a piece of BDAP encrypted ciphertext using a Ed25519 private-key seed.
- * 
- * @param vchPrivKeySeed The Ed25519 private-key seed, 32 bytes
- * @param vchCipherText The input BDAP ciphertext
- * @param vchData The decrypted output
- * @param strErrorMessage The string containing error-message in the event of failure
- * @return true on success
- * @return false on failure
+ * Encrypt(pubkeys, message)
+ * Buffer[] pubkeys
+ * Buffer message
+ * returns: Buffer cipherText
+ **/
+NAN_METHOD(Encrypt) {
+    Nan::HandleScope scope;
 
+    v8::Local<v8::Value> bufferObj;
+    if (info.Length() < 2) {
+        return Nan::ThrowError("Encrypt requires at least two parameters");
+    }
+
+    v8::Local<v8::Object> messageObj;
+    if (!info[1]->ToObject(Nan::GetCurrentContext()).ToLocal(&messageObj) || !Buffer::HasInstance(messageObj)) {
+        return Nan::ThrowError("Encrypt second parameter [message] error");
+    }
+    v8::Local<v8::Object> cipherText = Nan::NewBuffer(64).ToLocalChecked();
+    info.GetReturnValue().Set(cipherText);
+}
+/*
 bool DecryptBDAPData(const CharVector& vchPrivKeySeed,
                      const CharVector& vchCipherText,
                      CharVector& vchData,
                      std::string& strErrorMessage);
  */
-void decrypt(const v8::FunctionCallbackInfo<v8::Value>& args) {
-    v8::Isolate* isolate = args.GetIsolate();
+ /**
+ * Decrypt(privatekey, cipherText)
+ * Buffer privatekey
+ * Buffer cipherText
+ * returns: Buffer clearText
+ **/
+NAN_METHOD(Decrypt) {
+    Nan::HandleScope scope;
 
-    if (args.Length() < 2) {
-       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
-       return;
+    v8::Local<v8::Value> bufferObj;
+    if (info.Length() < 2) {
+        return Nan::ThrowError("Decrypt requires at least two parameters");
     }
-    /*
-    // Get Private Key Seed Argument
-    Local<Object> objPrivKeySeed = args[0]->ToObject();
-    if(!Buffer::HasInstance(objPrivKeySeed)) {
-       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate," Private Key Seed Argument should be a buffer object.")));
-       return;
+    // get private key parameter
+    v8::Local<v8::Object> privateKeyObj;
+    if (!info[0]->ToObject(Nan::GetCurrentContext()).ToLocal(&privateKeyObj) || !Buffer::HasInstance(privateKeyObj)) {
+        return Nan::ThrowError("Decrypt first parameter [privatekey] error");
     }
-    const std::vector<unsigned char> vchPrivKeySeed = (std::vector<unsigned char>)Buffer::Data(objPrivKeySeed);
-    // Get CipherText Argument
-    Local<Object> objCipherText = args[1]->ToObject();
-    if(!Buffer::HasInstance(objCipherText)) {
-       isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate,"Cipher Text Argument should be a buffer object.")));
-       return;
+    // get cipher text parameter
+    v8::Local<v8::Object> cipherTextObj;
+    if (!info[1]->ToObject(Nan::GetCurrentContext()).ToLocal(&cipherTextObj) || !Buffer::HasInstance(cipherTextObj)) {
+        return Nan::ThrowError("Decrypt second parameter [cipher text] error");
     }
-    const std::vector<unsigned char> vchCipherText = (std::vector<unsigned char>)Buffer::Data(objCipherText);
-
-    std::vector<unsigned char> output;
-    std::string strErrorMessage;
-    if !(DecryptBDAPData(vchPrivKeySeed, vchCipherText, output, strErrorMessage)) {
-        isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, strErrorMessage)));
-        return;
-    }
-    */
-    unsigned char* output = (unsigned char*) malloc(sizeof(unsigned char) * 32);
-    v8::Local<v8::Value> returnValue = Nan::CopyBuffer(((char*)output), 32).ToLocalChecked();
-    args.GetReturnValue().Set(returnValue);
+    v8::Local<v8::Object> clearText = Nan::NewBuffer(64).ToLocalChecked();
+    info.GetReturnValue().Set(clearText);
 }
 
-void init(v8::Local<v8::Object> target) {
-    NODE_SET_METHOD(target, "Encrypt", encrypt);
-    NODE_SET_METHOD(target, "Decrypt", decrypt);
+void InitModule(v8::Local<v8::Object> exports) {
+    Nan::SetMethod(exports, "Encrypt", Encrypt);
+    Nan::SetMethod(exports, "Decrypt", Decrypt);
 }
 
-NODE_MODULE(vgp, init)
+NODE_MODULE(vgp, InitModule)
